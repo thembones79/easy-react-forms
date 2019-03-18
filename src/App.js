1c55795068dd5388cdddf34addbf814ae200836f
thembones79 <michal.szumnarski@gmail.com>
2019-03-18 16:55:55 +0100

    added controlled form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    name: "",
+    company: "",
+    email: "",
+    phone: "",
+    message: ""
+  };
+
+  handleNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
+  handleCompanyChange = event => {
+    this.setState({ company: event.target.value });
+  };
+  handleEmailChange = event => {
+    this.setState({ email: event.target.value });
+  };
+  handlePhoneChange = event => {
+    this.setState({ phone: event.target.value });
+  };
+  handleMessageChange = event => {
+    this.setState({ message: event.target.value });
+  };
+
+  handleSubmit = async () => {
+    const { name, company, email, phone, message } = this.state;
+    try {
+      await axios.post(`https://afternoon-hamlet-86579.herokuapp.com/send`, {
+        name,
+        company,
+        email,
+        phone,
+        message
+      });
+    } catch (err) {
+      //      console.log(err);
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <form>
           <p>
-            Edit <code>src/App.js</code> and savey to reload.
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              value="name"
+              onChange={handleNameChange}
+            />
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <p>
+            <label>Company</label>
+            <input
+              type="text"
+              name="company"
+              value="company"
+              onChange={handleCompanyChange}
+            />
+          </p>
+          <p>
+            <label>Email Address</label>
+            <input
+              type="email"
+              name="email"
+              value="email"
+              onChange={handleEmailChange}
+            />
+          </p>
+          <p>
+            <label>Phone Number</label>
+            <input
+              type="text"
+              name="phone"
+              value="phone"
+              onChange={handlePhoneChange}
+            />
+          </p>
+          <p>
+            <label>Message</label>
+            <textarea
+              name="message"
+              rows="5"
+              value="message"
+              onChange={handleMessageChange}
+            />
+          </p>
+          <p>
+            <button onClick={handleSubmit}>Submit</button>
+          </p>
+        </form>
       </div>
     );
   }
